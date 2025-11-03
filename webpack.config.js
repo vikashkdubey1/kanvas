2ebcda6b75cd7e98b3bd9d6c81d567c7f14902e8
vikashkdubey1 <vikashkdubey1@outlook.com>
2025-11-03 19:45:04 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -36,4 +36,5 @@ module.exports = {
   port: process.env.PORT || 3000, // Render dynamically assigns a port
   allowedHosts: 'all',            // allow Render’s hostname
   historyApiFallback: true,       // for React Router / SPA support
+  }
 };
